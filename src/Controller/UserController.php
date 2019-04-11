439fa4af9c365ee115c2bf21d1370a9e41a15d4b
Yoann Laissus <yoann.laissus@gmail.com>
2019-04-11 22:14:54 +0200

    Add user entry points

---
+++ src/Controller/UserController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use Symfony\Component\Routing\Annotation\Route;
+use Swagger\Annotations as SWG;
+
+class UserController extends AbstractFOSRestController
+{
+    /**
+     *  @SWG\Response(
+     *     response=200,
+     *     description="Returns the status of the user",
+     *     @SWG\Schema(
+     *        type="object",
+     *        @SWG\Property(property="logged", type="boolean", description="If the user is connected or not"),
+     *        @SWG\Property(property="oauth_url", type="string", description="The OAuth connection URL"),
+     *        @SWG\Property(property="user", type="object", description="The user object", @SWG\Property(property="username", type="string", description="The username of the user"))
+     *     )
+     * )
+     *
+     * @Route("/user/status", name="user_status", methods={"GET"})
+     */
+    public function loginStatus()
+    {
+        $user = $this->getUser();
+
+        $response = [
+            'logged' => $user !== null,
+            'user' => null,
+            'oauth_url' => $this->generateUrl('hwi_oauth_service_redirect', ['service' => 'custom'])
+        ];
+
+        if ($user) {
+            $response['user'] = [
+                'username' => $user->getUsername()
+            ];
+        }
+
+        return $this->json($response);
+    }
+}
