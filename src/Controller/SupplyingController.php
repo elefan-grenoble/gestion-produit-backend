3c7af551fc42815536014e2870413654059b2dbb
Bastien Deshayes <bastien.deshayes@kelkoogroup.com>
2019-08-12 00:41:10 +0200

    Create supplying

--- src/Controller/SupplyingController.php
+++ src/Controller/SupplyingController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use App\Entity\Supplying;
 use FOS\RestBundle\Context\Context;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Nelmio\ApiDocBundle\Annotation\Model;
+use phpDocumentor\Reflection\Types\Integer;
 use Swagger\Annotations as SWG;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @Rest\Route("/supplying")
@@ -32,8 +35,40 @@ class SupplyingController extends AbstractFOSRestController
     public function getSupplying()
     {
         $em = $this->getDoctrine();
-        $reappros = $em->getRepository(Supplying::class)->findAll();
-        return $this->view($reappros)->setContext($this->getContext());
+        $supplyings = $em->getRepository(Supplying::class)->findAll();
+        return $this->view($supplyings)->setContext($this->getContext());
+    }
+
+    /**
+     * @Rest\Post("/{id}/quantity/{quantity}")
+     * @Rest\View(statusCode = 201)
+     *
+     * @SWG\Response(
+     *     response=201,
+     *     description="Returns the supplying created",
+     *     @SWG\Schema(
+     *         type="array",
+     *         @SWG\Items(ref=@Model(type=Supplying::class))
+     *     )
+     * )
+     *
+     * @SWG\Tag(name="supplying")
+     */
+    public function createSupplying(Article $article, int $quantity)
+    {
+        $supplying = new Supplying();
+        $supplying->setArticle($article);
+        $supplying->setCreationDate(new \DateTime());
+        $supplying->setQuantity($quantity);
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $em->persist($supplying);
+        $em->flush();
+
+        return $this->view(
+            $supplying,
+            Response::HTTP_CREATED
+        );
     }
 
     private function getContext()
